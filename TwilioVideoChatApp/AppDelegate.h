178134e400867f0af8f85c260a853df7ab224249
Anant Kannaik <anant.kannaik@creativecapsule.com>
2018-08-21 09:44:23 +0530

    Initial commit

---
+++ TwilioVideoChatApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwilioVideoChatApp
+//
+//  Created by Anant Kannaik on 21/08/18.
+//  Copyright © 2018 Anant Kannaik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwilioVideoChatApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwilioVideoChatApp
+//
+//  Created by Anant Kannaik on 21/08/18.
+//  Copyright © 2018 Anant Kannaik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
